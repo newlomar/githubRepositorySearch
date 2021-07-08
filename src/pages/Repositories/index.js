8259d60ccb6607a24c1c6eb1241497fa7ad64c68
Newton Lomar <newtonlomar16@gmail.com>
2021-07-08 09:14:03 -0300

    first commit

---
+++ src/pages/Repositories/index.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import * as S from './styled';
+
+function Repositories() {
+
+    const [ repositories, setRepositories ] = useState([]);
+
+    useEffect(() => {
+        let repositoriesName = localStorage.getItem('repositoriesName');
+        repositoriesName = JSON.parse(repositoriesName);
+        setRepositories(repositoriesName);
+        localStorage.clear();
+    }, []);
+
+    return (
+        <S.Container>
+            <S.Title>Repositórios</S.Title>
+            <S.List>
+                { 
+                    repositories.map(repository => {
+                        return (
+                            <S.ListItem>Repositório: { repository }</S.ListItem>
+                        )
+                    }) 
+                }
+            </S.List>
+        </S.Container>
+    );
+}
+
+export default Repositories;
